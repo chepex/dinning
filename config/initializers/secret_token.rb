5f473746700eeb60e6c239e2b8ab18df8b69dffa
Mario <mmixco@caricia.com>
2012-09-11 07:54:24 -0600

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinning::Application.config.secret_token = '3199903ab4c228c5459c640c91dfcc797fedf5c6f16a9a3718e77dc95c0432d2739e96410c2d8d4eed0d85a5cbe816de2b93ed2d0efe2ec8c7784bc37f45f4a9'
